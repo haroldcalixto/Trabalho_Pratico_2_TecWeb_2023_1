ec38d08936ab2fa0f56b2a78efe53ab16b956597
Harold Calixto <104173291+haroldcalixto@users.noreply.github.com>
2023-07-07 16:59:02 -0300

    Add files via upload

---
+++ css/style.css
@@ -0,0 +1,14 @@
+/*
+    Trabalho Prático 2 - HTML/CSS/JS - 11/07/2023;
+    Disciplina: Tecnologias Web;
+    Professor: Fischer Ferreira;
+    Semestre: 2023.1;
+
+    Equipe:
+    Aluno: Harold Calixto de Albuquerque - Matrícula: 499735;
+    Aluno: Alexandre Gomes Camelo - Matrícula: 504315;
+    Aluno: Joaquim Walisson Portela de Sousa - Matrícula: 472152;
+
+    Projeto: Desenvolvimento do Jogo Enduro - Atari;
+*/
+
